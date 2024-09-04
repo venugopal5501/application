5cca15c4a2c62f747004ee21afb0c281404e0758
venugopal5501 <venugopal14082002@gmail.com>
2024-09-04 13:00:12 +0530

    CounterPage

--- src/components/CounterPage.vue
+++ src/components/CounterPage.vue
@@ -1,29 +1,35 @@
 <template>
     <h1>Counter Page</h1>
     <h2>{{ data }}</h2>
-    <button v-on:click="increment">Increment</button>
-    <button v-on:click="decrement">Decrement</button>
-    <button v-on:click="doubleInc">Increment By 5</button>
+    <button v-on:click="clickingInc">Increment</button>
+    <button v-on:click="clickingDec">Decrement</button>
+    <button v-on:click="IncBy5(20)">Increment By 5</button>
 </template>
 <script>
 export default {
     name: 'CounterPage',
-    data() {
+    // data() {
+    //     return {
+    //         data: 0,
+    //         value: 5
+    //     }
+    // },
+    data: function () {
         return {
             data: 0,
             value: 5
         }
     },
     methods: {
-        increment() {
+        clickingInc: function () {
             this.data = this.data + 1
         },
-        decrement() {
+        clickingDec: function () {
             this.data = this.data - 1;
 
         },
-        doubleInc() {
-            this.data = this.data + this.value
+        IncBy5:function(value) {
+            this.data = this.data + value
         }
     }
 }
