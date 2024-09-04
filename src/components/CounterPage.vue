42b2ff3084431c5cd469efd02372e363baa4001c
venugopal5501 <venugopal14082002@gmail.com>
2024-09-04 12:55:28 +0530

    navbar

--- src/components/CounterPage.vue
+++ src/components/CounterPage.vue
@@ -1,9 +1,31 @@
 <template>
     <h1>Counter Page</h1>
+    <h2>{{ data }}</h2>
+    <button v-on:click="increment">Increment</button>
+    <button v-on:click="decrement">Decrement</button>
+    <button v-on:click="doubleInc">Increment By 5</button>
 </template>
 <script>
 export default {
     name: 'CounterPage',
+    data() {
+        return {
+            data: 0,
+            value: 5
+        }
+    },
+    methods: {
+        increment() {
+            this.data = this.data + 1
+        },
+        decrement() {
+            this.data = this.data - 1;
+
+        },
+        doubleInc() {
+            this.data = this.data + this.value
+        }
+    }
 }
 </script>
 <style></style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,14 @@
 <template>
-    
+
     <router-link to="/counter">counter </router-link>|
     <router-link to="/employee">Employees </router-link>|
     <router-link to="/users">UserList </router-link>
-    <router-view/>
-    
+    <router-view />
+
 </template>
 <script>
-export default{
+export default {
     name: 'NavBar'
 }
 </script>
-<style>
-</style>
+<style></style>
