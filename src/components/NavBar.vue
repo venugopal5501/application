3b9fd8d9bccf62b7043f4b2d8661ae90894f6133
venugopal5501 <venugopal14082002@gmail.com>
2024-09-04 17:46:54 +0530

    commit

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-
+    <router-link to="/"><img src="../assets/logo.png" width="50" ></router-link>|
     <router-link to="/counter">counter </router-link>|
     <router-link to="/employee">Employees </router-link>|
     <router-link to="/users">UserList </router-link>
